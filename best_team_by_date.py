20a14543e6f8e9bb273a8739e1f56e120eeb6576
float64 <float64>
2016-07-20 21:02:05 -0400

    convert best_team_by_date into a function

--- best_team_by_date.py
+++ best_team_by_date.py
@@ -5,34 +5,37 @@ import mobile_companion.team_optimizer.meta as META
 import copy, pickle
 from datetime import timedelta
 
-outfile = 'best_overall.dat'
-res = dict()
-calc = MCC.Standard('../standard_weights.dat')
 
-obj_func = lambda x: calc.calculate(x)[1]['overall']['rounded']
 
-player_db = MDB.import_database_csv('../player_database.csv')
-dates = sorted(set([p.date_added for p in player_db]))
+def calc_best_teams(obj_type='overall'):
+    outfile = 'best_{}.dat'.format(obj_type)
+    
+    res = dict()
+    calc = MCC.Standard('../standard_weights.dat')
 
-start_date = dates[0]
-end_date = dates[-1]
+    obj_func = lambda x: calc.calculate(x)[1][obj_type]['rounded']
 
-print("Running analysis on dates {} to {}".format(start_date, end_date))
+    player_db = MDB.import_database_csv('../player_database.csv')
+    dates = sorted(set([p.date_added for p in player_db]))
 
-current_date = start_date
-prior_best_team = None
+    start_date = dates[0]
+    end_date = dates[-1]
 
-while current_date <= end_date:
-    current_players = [ p for p in player_db if p.date_added <= current_date ]
-    print("Starting {} [ {} players ]".format(current_date, len(current_players)))
+    print("Running analysis on dates {} to {}".format(start_date, end_date))
 
-    best_team, best_obj = META.optimize(current_players, obj_func, initial_guess=prior_best_team)
+    current_date = start_date
+    prior_best_team = None
 
-    res[current_date] = (best_team, best_obj)
-    pickle.dump(res, open(outfile, 'wb'))
+    while current_date <= end_date:
+        current_players = [ p for p in player_db if p.date_added <= current_date ]
+        print("Starting {} [ {} players ]".format(current_date, len(current_players)))
 
-    current_date = current_date + timedelta(days=7)
-    prior_best_team = best_team
-    print("Best obj: {}".format(best_obj))
-    print("")
+        best_team, best_obj = META.optimize(current_players, obj_func, initial_guess=prior_best_team)
 
+        res[current_date] = (best_team, best_obj)
+        pickle.dump(res, open(outfile, 'wb'))
+
+        current_date = current_date + timedelta(days=7)
+        prior_best_team = best_team
+        print("Best obj: {}".format(best_obj))
+        print("")
